75ace06d4363e2d09eeece588863595b617e544d
Fernando Terra <fernando.terras@outlook.com>
2018-08-15 04:05:51 -0300

    Initial commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using br.com.sagradacruz.DAO;
+using br.com.sagradacruz.Models;
+
+namespace br.com.sagradacruz.Controllers
+{
+    public class LoginController : Controller
+    {      
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var model = new LoginViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Access(LoginViewModel model)
+        {
+            var dAO = new LoginDAO();
+            if (ModelState.IsValid)
+            {
+                if (dAO.Login(model))
+                {
+                    return View("Index", model); //RedirectToAction("Index", "Post", new { area = "Admin" });
+                }
+                else
+                {
+                    return View("Index", new LoginViewModel());
+                }                
+            }
+            else
+            {
+                return View("Index", new LoginViewModel());
+            }
+        }
+    }
+}
---
+++ DAO/LoginDAO.cs
@@ -0,0 +1,12 @@
+﻿using br.com.sagradacruz.Models;
+
+namespace sagradacruz.com.br.DAO
+{
+    public class LoginDAO
+    {
+        public bool Login(LoginViewModel user)
+        {
+            return true;
+        }
+    }
+}
